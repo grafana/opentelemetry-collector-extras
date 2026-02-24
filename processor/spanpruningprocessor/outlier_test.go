47f0be6a274fa1fba1c4f0a263fb90d6d0841258
Sean Porter <portertech@gmail.com>
2026-02-24 14:52:10 -0800

    Remove import comment from test file

--- processor/spanpruningprocessor/outlier_test.go
+++ processor/spanpruningprocessor/outlier_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package spanpruningprocessor // import "github.com/grafana/opentelemetry-collector-extras/processor/spanpruningprocessor"
+package spanpruningprocessor
 
 import (
 	"testing"
